f000b11ef88f94fbc7379c40c0f921044f059272
Max Frayd <maxfrayd@Maxs-MacBook-Air.local>
2022-03-29 16:15:27 +0200

    fixed intial state for currentUser

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,4 +1,4 @@
-const currentUser = (state, action) => {
+const currentUser = (state = {}, action) => {
   switch (action.type) {
     case 'login': {
       const { userEmail, allUsers } = action.payload;
